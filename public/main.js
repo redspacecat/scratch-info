d39f9d1ed857e4ff5856a123bd5fc7a7fd0fb904
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-05-23 22:08:13 -0700

    fixed navigating forward/backward

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", setup3)
 
 function setup3() {
+    // add topbar loading bar
     let s = document.createElement("script")
     s.src = "https://cdn.jsdelivr.net/gh/buunguyen/topbar@latest/topbar.min.js"
     s.onload = function() {
@@ -19,9 +20,20 @@ function setup3() {
         }
     })
 
+    // add vercel analytics
     window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
     let s2 = document.createElement("script")
     s2.src = "/_vercel/insights/script.js"
     s2.defer = true
     document.head.appendChild(s2)
+
+    // fix history back and forth
+    // window.onpopstate = function() {
+    // if(window.performance.navigation.type === 2) {
+    setInterval(function() {
+        if (String(window.performance.getEntriesByType("navigation")[0].type) === "back_forward") {
+            console.log("went back/forward")
+            setTimeout(topbar.hide, 100)
+        }
+    }, 500)
 }
