d00c00f3c1b2e05daf42abfdaecbca17d02ebea2
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-05-23 20:50:31 -0700

    add progress bar when loading

--- public/main.js
+++ public/main.js
@@ -3,6 +3,19 @@ window.addEventListener("DOMContentLoaded", setup3)
 function setup3() {
     let s = document.createElement("script")
     s.src = "https://cdn.jsdelivr.net/gh/buunguyen/topbar@latest/topbar.min.js"
+    s.onload = function() {
+        topbar.config({autoRun: false})
+        topbar.show()
+        topbar.progress(0.7)
+        topbar.hide()
+        topbar.config({autoRun: true})
+    }
     document.head.appendChild(s)
-    document.querySelectorAll("a").forEach(a => a.addEventListener("click", function() {topbar.show()}))
+    document.querySelectorAll("a").forEach(function(a) {
+        if (a.target != "_blank") {
+            a.addEventListener("click", function() {
+                topbar.show()
+            })
+        }
+    })
 }
